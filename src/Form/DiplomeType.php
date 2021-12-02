6968e1555f533b727ca55f4bc93d5fad25ee3210
Yannick B <yannickbiheul@gmail.com>
2021-12-02 14:11:49 +0100

    Création du CRUD Diplomes

---
+++ src/Form/DiplomeType.php
@@ -0,0 +1,51 @@
+<?php
+
+namespace App\Form;
+
+use App\Entity\Diplome;
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\Extension\Core\Type\DateType;
+use Symfony\Component\Form\Extension\Core\Type\SubmitType;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\OptionsResolver\OptionsResolver;
+
+class DiplomeType extends AbstractType
+{
+    public function buildForm(FormBuilderInterface $builder, array $options): void
+    {
+        $builder
+            ->add('titre', TextType::class, [
+                'label' => 'Titre du diplôme',
+                'attr' => [
+                    'placeholder' => 'Titre'
+                ]
+            ])
+            ->add('etablissement', TextType::class, [
+                'label' => 'Établissement du diplôme',
+                'attr' => [
+                    'placeholder' => 'Établissement'
+                ]
+            ])
+            ->add('dateObtention', DateType::class, [
+                'label' => 'Date du diplôme',
+                'attr' => [
+                    'placeholder' => 'Date'
+                ]
+            ])
+            ->add('submit', SubmitType::class, [
+                'label' => 'Enregistrer',
+                'attr' => [
+                    'class' => 'btn btn-info text-white'
+                ]
+            ])
+        ;
+    }
+
+    public function configureOptions(OptionsResolver $resolver): void
+    {
+        $resolver->setDefaults([
+            'data_class' => Diplome::class,
+        ]);
+    }
+}
